5ea6c38002d864fbf1b4d6cfa730e0e0a557686f
Weder Sousa <weder96@gmail.com>
2025-02-27 17:53:12 -0300

    :rocket: adicionar database store

---
+++ databases/store/DDL.sql
@@ -0,0 +1,72 @@
+CREATE TABLE "Employees" (
+	"EmployeeID" "int" IDENTITY (1, 1) NOT NULL ,
+	"LastName" nvarchar (20) NOT NULL ,
+	"FirstName" nvarchar (10) NOT NULL ,
+	"Title" nvarchar (30) NULL ,
+	"TitleOfCourtesy" nvarchar (25) NULL ,
+	"BirthDate" "datetime" NULL ,
+	"HireDate" "datetime" NULL ,
+	"Address" nvarchar (60) NULL ,
+	"City" nvarchar (15) NULL ,
+	"Region" nvarchar (15) NULL ,
+	"PostalCode" nvarchar (10) NULL ,
+	"Country" nvarchar (15) NULL ,
+	"HomePhone" nvarchar (24) NULL ,
+	"Extension" nvarchar (4) NULL ,
+	"Photo" "image" NULL ,
+	"Notes" "ntext" NULL ,
+	"ReportsTo" "int" NULL ,
+	"PhotoPath" nvarchar (255) NULL ,
+	PRIMARY KEY("EmployeeID")
+)
+
+
+CREATE TABLE "Categories" (
+	"CategoryID" "int" IDENTITY (1, 1) NOT NULL ,
+	"CategoryName" nvarchar (15) NOT NULL ,
+	"Description" "ntext" NULL ,
+	"Picture" "image" NULL ,
+	PRIMARY KEY("CategoryID")
+)
+
+
+CREATE TABLE "Customers" (
+	"CustomerID" nchar (5) NOT NULL ,
+	"CompanyName" nvarchar (40) NOT NULL ,
+	"ContactName" nvarchar (30) NULL ,
+	"ContactTitle" nvarchar (30) NULL ,
+	"Address" nvarchar (60) NULL ,
+	"City" nvarchar (15) NULL ,
+	"Region" nvarchar (15) NULL ,
+	"PostalCode" nvarchar (10) NULL ,
+	"Country" nvarchar (15) NULL ,
+	"Phone" nvarchar (24) NULL ,
+	"Fax" nvarchar (24) NULL ,
+	PRIMARY KEY("CustomerID")
+)
+
+CREATE TABLE "Products" (
+	"ProductID" "int" IDENTITY (1, 1) NOT NULL ,
+	"ProductName" nvarchar (40) NOT NULL ,
+	"SupplierID" "int" NULL ,
+	"CategoryID" "int" NULL ,
+	"QuantityPerUnit" nvarchar (20) NULL ,
+	"UnitPrice" "money" NULL CONSTRAINT "DF_Products_UnitPrice" DEFAULT (0),
+	"UnitsInStock" "smallint" NULL CONSTRAINT "DF_Products_UnitsInStock" DEFAULT (0),
+	"UnitsOnOrder" "smallint" NULL CONSTRAINT "DF_Products_UnitsOnOrder" DEFAULT (0),
+	"ReorderLevel" "smallint" NULL CONSTRAINT "DF_Products_ReorderLevel" DEFAULT (0),
+	"Discontinued" "bit" NOT NULL CONSTRAINT "DF_Products_Discontinued" DEFAULT (0),
+	PRIMARY KEY  ("ProductID")
+)
+
+
+CREATE TABLE "OrderDetails" (
+	"OrderDetailsID" "int" IDENTITY (1, 1) NOT NULL ,
+	"OrderID" "int" NOT NULL ,
+	"ProductID" "int" NOT NULL ,
+	"UnitPrice" "money" NOT NULL CONSTRAINT "DF_Order_Details_UnitPrice" DEFAULT (0),
+	"Quantity" "smallint" NOT NULL CONSTRAINT "DF_Order_Details_Quantity" DEFAULT (1),
+	"Discount" "real" NOT NULL CONSTRAINT "DF_Order_Details_Discount" DEFAULT (0),
+	PRIMARY KEY("OrderDetailsID"),
+	FOREIGN KEY ("ProductID") REFERENCES "dbo"."Products" ("ProductID")
+)
